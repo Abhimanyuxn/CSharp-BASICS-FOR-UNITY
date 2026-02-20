f99898c6868dd63eb95715063aefa7bae2548166
Abhimanyu <abhimanyu21chauhan@gmail.com>
2026-02-21 00:04:58 +0530

    Add files via upload

---
+++ datatype.cs
@@ -0,0 +1,13 @@
+using System;
+class program
+{
+    static void Main (string []args)
+    {
+        string name = "hill";
+        char grade = 'A';
+        int a= 10;
+        float money=62.8f;
+        Console.WriteLine("Game name is " + name + " " + money);
+        Console.WriteLine("Grade is " + grade);
+    }
+}
