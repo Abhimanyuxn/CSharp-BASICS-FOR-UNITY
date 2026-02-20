ecbd1986c2d521efcae9adc0d6590f26b7ce0383
Abhimanyu <abhimanyu21chauhan@gmail.com>
2026-02-21 00:29:28 +0530

    Add files via upload

---
+++ boxes.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = 19;
+            Console.Write(Number);
+        }
+    }
+}
