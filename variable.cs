c73d0496e01f561b2fda56cd381ab76be93a4757
Abhimanyu <abhimanyu21chauhan@gmail.com>
2026-02-20 23:46:27 +0530

    Add files via upload

---
+++ variable.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Hill Climb";
+        int a = 10;
+
+        Console.WriteLine("Game name is " + name + " " + a);
+    }
+}
