e8756922c0a382ff72403e08dbf35f1006d10c0e
Abhimanyu <abhimanyu21chauhan@gmail.com>
2026-02-21 15:24:34 +0530

    Add files via upload

---
+++ input.cs
@@ -0,0 +1,10 @@
+using System;
+class program 
+{
+static void Main(string[] args)
+{
+    Console.Write("Enter your name : ");
+    string name = Console.ReadLine();
+         Console.WriteLine("Hello  " + name);
+}
+}
